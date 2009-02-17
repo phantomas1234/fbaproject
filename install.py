ddaf598735b8e2f141d8c002c1ff0e47ce2cfc6e
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2009-02-17 13:52:18 +0100

    The install script (install.py) compiles and installs the included glpk version.
    The glpk dir gets cleaned up after the installation (make clean and make distclean)

--- install.py
+++ install.py
@@ -12,6 +12,7 @@ import os
 
 
 def main():
+    os.system('cd glpk-4.36; ./configure; make; make install; make clean; make distclean')
     os.system('python setup.py install --prefix=$HOME/privateRoot')
     os.system('cd build; rm -R *')
 
