cb4280f2df55b42f516f288e5067add6ccc6e628
Nikolaus Sonnenschein <niko@magenta.iuhb02.iu-bremen.de>
2009-03-06 08:28:43 +0100

    Context FBA for Arndts data

--- ifba/ContextFBA/ContextFBA.py
+++ ifba/ContextFBA/ContextFBA.py
@@ -15,9 +15,28 @@ from ifba.general.combinatorics import SetCombine
 import copy
 import random
 
+class ExpressionData2ContextData(object):
+    """Translates a set of expression probabilities into a weighted reaction
+    set. It can be used for ContextFBA. It needs a logical mapping between 
+    reactions and probe IDs.
+    """
+    def __init__(self, logical_rxn_probeID_map):
+        super(ExpressionData2ContextData, self).__init__()
+        self.logical_rxn_probeID_map = logical_rxn_probeID_map
+        
+
 
 class ContextFBA(metabolism.Metabolism):
-    """docstring for ClassName"""
+    """Implementation of Context-specific FBA.
+    
+    lp -> glpk problem struct
+    rmfID -> Specification of the cell objective
+    level -> The percentage of the normal FBA objective flux that should be
+        fixed in the following contextFBA simulations
+    
+    Becker und Palsson. Context-specific metabolic networks are consistent
+    with experiments. PLoS Comput Biol (2008) vol. 4 (5) pp. e1000082
+    """
     def __init__(self, lp, rmfID=None, level=.9):
         metabolism.Metabolism.__init__(self, lp)
         self.rmfID = rmfID
@@ -50,6 +69,7 @@ class ContextFBA(metabolism.Metabolism):
         return inconsistency
     
     def contextFBA(self, expData, cutoff=None):
+        """Uses an weighted reaction set in form of a dictionary """
         contxtObj = self._generateContextObjective(expData, cutoff)
         self.setObjectiveFunction(contxtObj)
         self.setOptFlag("Min")
@@ -68,7 +88,7 @@ def loadReactionData(path):
 def main():
     dataPaths = tuple([file.rstrip() for file in os.popen("ls *.tsv")])
     
-    model_path = '../HumanFBA/validHumanFBAmodel.lp'
+    model_path = '../../work/HumanFBA/validHumanFBAmodel.lp'
     stdFBAfluxDist = metabolism.Metabolism(util.ImportCplex(model_path)).fba()
     print "Standard FBA fluxDist: ", stdFBAfluxDist.getActiveFluxDist()
     
@@ -89,7 +109,7 @@ def main():
             print float(len(reactionSets[i].intersection(reactionSets[j])))/float(max(len(reactionSets[i]),len(reactionSets[j])))
 
 def main2():
-    model_path = '../HumanFBA/validHumanFBAmodel.lp'
+    model_path = '../../work/HumanFBA/validHumanFBAmodel.lp'
     path = "pUC-18_EBER2-L2.tsv"
     cntxtFBA = ContextFBA(util.ImportCplex(model_path, terminal="ON"), rmfID='R("R_MCDm")', level=.8)
     reacs = cntxtFBA.getReactions()
@@ -100,115 +120,39 @@ def main2():
         print "results for: ", r, " for ", path
         # print "ContextFBA fluxDist: ", cntxtFluxDist.getActiveFluxDist()
         print "Inconsistency Score: ", incon
+        
+def main3(path, outpath):
+    dataPaths = tuple([file.rstrip() for file in os.popen("ls "+path+"*.tsv")])
+    print dataPaths
+    outputFile = open(outpath, 'w').close()
+    outputFile = open(outpath, 'a')
+    model_path = '../../work/HumanFBA/validHumanFBAmodel.lp'
+    stdFBAfluxDist = metabolism.Metabolism(util.ImportCplex(model_path)).fba()
+    print "Standard FBA fluxDist: ", stdFBAfluxDist.getActiveFluxDist()
+    
+    print 2*"\n"
+    reactionSets = list()
+    for path in dataPaths:
+        cntxtFBA = ContextFBA(util.ImportCplex(model_path), \
+        rmfID='R("R_Obj")', level=.8)
+        (cntxtFluxDist, incon) = cntxtFBA.contextFBA(loadReactionData(path), \
+        cutoff=.8)
+        print "results for: ", path
+        print "ContextFBA fluxDist: ", cntxtFluxDist.getActiveFluxDist()
+        print "Inconsistency Score: ", incon
+        print 2*'\n'
+        reactionSets.append(set(cntxtFluxDist.getActiveReactions()))
+        outputFile.write(path + '\t'+ str(incon) + "\n")
+    outputFile.close()
+    # for i in range(0, 3):
+    #     for j in range(0,3):
+    #         print dataPaths[i], dataPaths[j]
+    #         print float(len(reactionSets[i].intersection(reactionSets[j])))/float(max(len(reactionSets[i]),len(reactionSets[j])))
+
 
 
 if __name__ == '__main__':
-    main2()
+    print main3("/Users/niko/arbeit/Publishing/Arndt_Colab/data/contextData/EBER/", \
+    "/Users/niko/arbeit/Publishing/Arndt_Colab/data/contextData/eberResults.tsv")
     
 
-
-
-# Old Stuff
-
-# if __name__ == '__main__':
-#
-#     def posNegSplit(real):
-#         if real > 0.:
-#             return 1
-#         else:
-#             return -1
-#
-#     threshold1 = .8
-#
-#     dat = open('/Users/niko/arbeit/Publishing/MetabolicControlPaper2/code/test2.tsv', 'r')
-#     datDict = dict()
-#     for line in dat:
-#         tmp = line.rstrip().split('\t')
-#         datDict[tmp[0]] = float(tmp[1])
-#     dat.close()
-#
-#     datDict2 = dict([(key, -1 * value) for key, value in datDict.items()])
-#     print datDict2
-#
-#
-#     path = '../models/iAF1260template2.lp'
-#     lp = metabolism.Metabolism(util.ImportCplex(path))
-#
-#
-#     lp.setReactionObjectiveMinimizeRest('R("R_Ec_biomass_iAF1260_core_59p81M")')
-#
-#     transp = lp.getTransporters()
-#     superMedium = dict()
-#     for i in transp:
-#         superMedium[i] = (-20., 20.)
-#     # lp.modifyColumnBounds({'R("MglcDb_Transp")':(0, 20), 'R("R_ATPM")':(8.39, 8.39), 'R("Mo2b_Transp")':(0, 18.5)})
-#     lp.modifyColumnBounds(superMedium)
-#     lp2 = copy.copy(lp)
-#     fd = lp2.fba()
-#     growth = fd['R("R_Ec_biomass_iAF1260_core_59p81M")']
-#     print growth
-#
-#     lp3 = copy.copy(lp2)
-#     lp3.modifyColumnBounds({'R("R_Ec_biomass_iAF1260_core_59p81M")':(growth*threshold1, growth)})
-#
-#     lp3.setObjectiveFunction(datDict)
-#
-#     fd = lp3.fba()
-#     open("/Users/niko/arbeit/Data/fluxOne.tsv", 'w').write(fd.tsv())
-#     growth = fd['R("R_Ec_biomass_iAF1260_core_59p81M")']
-#     print growth
-#     tmp1= fd.getActiveReactions()
-#     tmp1.sort()
-#
-#
-#
-#     lp3.setObjectiveFunction(datDict2)
-#
-#     fd = lp3.fba()
-#     open("/Users/niko/arbeit/Data/fluxTwo.tsv", 'w').write(fd.tsv())
-#     growth = fd['R("R_Ec_biomass_iAF1260_core_59p81M")']
-#     print growth
-#     tmp2 = fd.getActiveReactions()
-#     tmp2.sort()
-#
-#
-#     print
-#     print set(tmp1).difference(set(tmp2)), "\n"
-#     print set(tmp2).difference(set(tmp1)),
-
-    
-    
-    # context-specific fba
-    #
-    # print help(filterDict)
-    # print filterDict({1:.1,2:.99},.3)
-    #
-    # threshold1 = .8
-    # threshold2 = .8
-    # path = '../models/iAF1260template2.lp'
-    # lp = metabolism.Metabolism(util.ImportCplex(path))
-    # surrogatData = dict([(r, random.uniform(0., 1.)) for r in lp.getReactions()])
-    # print surrogatData
-    # for key, value in surrogatData.items():
-    #     if value < .8:
-    #         surrogatData[key] = 1. - value
-    #     else:
-    #         surrogatData.pop(key)
-    # print surrogatData
-    #
-    #
-    # lp.setReactionObjectiveMinimizeRest('R("R_Ec_biomass_iAF1260_core_59p81M")')
-    # lp.modifyColumnBounds({'R("MglcDb_Transp")':(0, 20), 'R("R_ATPM")':(8.39, 8.39), 'R("Mo2b_Transp")':(0, 18.5)})
-    # lp2 = copy.copy(lp)
-    # fd = lp2.fba()
-    # growth = fd['R("R_Ec_biomass_iAF1260_core_59p81M")']
-    # print growth
-    # lp3 = copy.copy(lp2)
-    # lp3.modifyColumnBounds({'R("R_Ec_biomass_iAF1260_core_59p81M")':(growth*threshold1, growth)})
-    #
-    # lp3.setObjectiveFunction(surrogatData)
-    #
-    # fd = lp3.fba()
-    # growth = fd['R("R_Ec_biomass_iAF1260_core_59p81M")']
-    # print growth
-    # print lp3.getObjectiveFunction()
