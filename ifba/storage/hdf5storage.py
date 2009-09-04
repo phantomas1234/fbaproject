b718d7af03d859341ec16b0e719d6af6007bb5e9
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2009-09-04 18:29:48 +0200

    hdf5storage includes now some routines and classes for the storage of FBA simulations

---
+++ ifba/storage/hdf5storage.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+untitled.py
+
+Created by Nikolaus Sonnenschein on 2009-07-03.
+Copyright (c) 2009 Jacobs University of Bremen. All rights reserved.
+"""
+
+from tables import *
+
+def h5Container(fileName, lp, title=""):
+    """Constructs a h5 file container that is suitable for the storage of FBAsimulationResult objects."""
+    numReacs = lp.getNumCols()
+    class FBAsimulations(IsDescription):
+        fluxactivity = Float32Col(shape=(numReacs), pos=0)
+        lowerBounds = Float32Col(shape=(numReacs), pos=1)
+        upperBounds = Float32Col(shape=(numReacs), pos=2)
+        objective = Float32Col(shape=(numReacs), pos=3)
+        timeStamp = Float32Col(pos=4)
+        modelPath = StringCol(100, pos=5)
+    h5 = openFile(fileName, 'w')
+    h5.createArray(where=h5.root, name='rxnMapping', object=lp.getColumnIDs(), title='Index - ReactionID mapping')
+    h5.createTable(where=h5.root, description=FBAsimulations, name='simulations', title='Table containing the activities and media conditions for FBA simulations.')
+    h5.flush()
+    print h5
+    h5.close()
+    return fileName
+    
+class SimulationDB(object):
+    """docstring for SimulationDB"""
+    def __init__(self, h5file):
+        self.h5container = openFile(h5file, 'r+')
+    
+    def writeSimulationResult(self, simulationResult):
+        "Takes a SimulationResult object and store it to the database."
+        table = self.h5container.root.simulations
+        row = table.row
+        colNames = table.colnames
+        for col in colNames:
+            try:
+                attr = getattr(simulationResult, col)
+            except NameError:
+                print "Missing of a mandatory column field in the provided simulation result"
+                sys.exit(-1)
+            row[col] = attr
+        row.append()
+        self.h5container.flush()
+        
+    def retrieveSimulationResultByID(self):
+        """docstring for re"""
+        # raise NotImplementedError, "Do it looser!"
+        table = self.h5container.root.simulations
+        print table[1]
+        
+    def close(self):
+        """Close the database."""
+        self.h5container.flush()
+        self.h5container.close()
+        print "bye bye"
+        
+
+class FBAsimulationResult(object):
+    """A class that stores parameters and results of FBAsimulations."""
+    def __init__(self, fluxDist, columnBounds, objective, timestamp, modelPath):
+        self.fluxDist = fluxDist
+        self.columnBounds = columnBounds
+        self.fluxactivity = self.fluxDist.getFluxArray()
+        self.constraints = self.__getValuesByKeys(self.columnBounds, self.fluxDist.reactions)
+        self.lowerBounds = [l for l, u in self.constraints]
+        self.upperBounds = [u for l, u in self.constraints]
+        self.objective = self.__getValuesByKeys(objective, self.fluxDist.reactions)
+        self.timeStamp = timestamp
+        self.modelPath = modelPath
+        
+    def __getValuesByKeys(self, dictionary, keys):
+        """Stupid function that returns values from a dict in the order specified by keys."""
+        tmpList = list()
+        for key in dictionary:
+            tmpList.append(dictionary[key])
+        return tuple(tmpList)
+
+    def __str__(self):
+        """Print information about simulation result."""
+        print "Timestamp (localtime):\n", time.asctime(time.localtime(self.timestamp))
+        print "\n"
+        print "Model path:\n", self.modelPath
+        print "\n"
+        print "Constraints:\n\n"
+        pprint.pprint(self.columnBounds)
+        print "\n"
+        print "Objective:\n"
+        pprint.pprint(self.objective)
+        print "\n"
+        print "Active Fluxdistribution:\n", self.fluxDist.active_tsv()
+        return ""
+
+
+if __name__ == '__main__':
+    pass
