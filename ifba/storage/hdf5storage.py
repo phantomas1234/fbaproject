a0a975f30b0c7829f918591a8402635380bf161d
Nikolaus Sonnenschein <niko@magenta.iuhb02.iu-bremen.de>
2009-09-11 10:21:52 +0200

    h5Container does not overwright an existing h5 file now

--- ifba/storage/hdf5storage.py
+++ ifba/storage/hdf5storage.py
@@ -7,11 +7,16 @@ Created by Nikolaus Sonnenschein on 2009-07-03.
 Copyright (c) 2009 Jacobs University of Bremen. All rights reserved.
 """
 
+import os
 from tables import *
 from ifba.GlpkWrap.fluxdist import FBAsimulationResult
 
+
 def h5Container(fileName, lp, title="", complevel=5, expectedrows=1000000):
     """Constructs a h5 file container that is suitable for the storage of FBAsimulationResult objects."""
+    if os.path.exists('fileName'):
+        return fileName
+    h5 = openFile(fileName, 'w')
     numReacs = lp.getNumCols()
     class FBAsimulations(IsDescription):
         fluxactivity = Float32Col(shape=(numReacs), pos=0)
@@ -20,7 +25,6 @@ def h5Container(fileName, lp, title="", complevel=5, expectedrows=1000000):
         objective = Float32Col(shape=(numReacs), pos=3)
         timeStamp = Float32Col(pos=4)
         modelPath = StringCol(100, pos=5)
-    h5 = openFile(fileName, 'w')
     h5.createArray(where=h5.root, name='rxnMapping', object=lp.getColumnIDs(), title='Index - ReactionID mapping')
     filters = Filters(complevel=5, complib='zlib', fletcher32=True, shuffle=True)
     h5.createTable(where=h5.root, description=FBAsimulations, name='simulations', title='Table containing the activities and media conditions for FBA simulations.', filters=filters, expectedrows=expectedrows)
